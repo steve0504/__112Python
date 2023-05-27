ecd4656eff71ba3d5d3934c9e7cf9fffe170f210
steve0504 <132725497+steve0504@users.noreply.github.com>
2023-05-27 05:55:36 +0000

    1

--- lesson8_1.py
+++ lesson8_1.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 response = requests.request('GET',url)
 if response.status_code == 200:
@@ -10,10 +11,6 @@ else:
 
 
 
-import pandas as pd
-
 dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
-dataFrame
-
-
-
+mask = dataFrame['sbi'] <= 3
+print(dataFrame[mask])
