955914e41eaf67d28616cb96d2c6aef8b0b1dfeb
steve0504 <132725497+steve0504@users.noreply.github.com>
2023-05-27 05:52:32 +0000

    1

---
+++ lesson8_1.py
@@ -0,0 +1,19 @@
+import requests
+url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+response = requests.request('GET',url)
+if response.status_code == 200:
+    print("連線成功")
+    all_data = response.json()
+    print(type(all_data))
+else:
+    print(f"連線失敗:{response.status_code}")
+
+
+
+import pandas as pd
+
+dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
+dataFrame
+
+
+
