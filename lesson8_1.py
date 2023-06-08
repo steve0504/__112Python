e7ef6c0f128976a6b838127f8025d2fcbba7b2f1
steve0504 <jason0504ju@gmail.com>
2023-06-09 00:04:46 +0800

    1

--- lesson8_1.py
+++ lesson8_1.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import streamlit as st
+
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 response = requests.request('GET',url)
 if response.status_code == 200:
@@ -12,9 +14,6 @@ else:
 
 
 dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
-
-mask = dataFrame['sbi'] <= min
+mask = dataFrame['sbi'] <= 3
 mask_dataFrame = dataFrame[mask]
-
-mask_dataFrame.to_excel(filename)
-
+st.dataframe(mask_dataFrame)
